eb02f124dc3d917ba284c85c032bdb50bbc55111
OlgaPyryeva <vashkevicholga@gmail.com>
2023-08-29 20:47:53 +0300

    feat: make class for guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,32 @@
 class GuessingGame {
-    constructor() {}
+  constructor(number) {
+    this.number = number;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+    let middle = this.min + Math.round((this.max - this.min) / 2);
+    this.middle = middle;
+    return this.middle;
+  }
 
-    }
+  guess() {
+    console.log(this.middle);
+    return this.middle;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.middle;
+    console.log(this.min, this.max, "lower");
+    return this.setRange(this.min, this.max);
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.middle;
+    console.log(this.min, this.max, "greater");
+    return this.setRange(this.min, this.max);
+  }
 }
 
 module.exports = GuessingGame;
